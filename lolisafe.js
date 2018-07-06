7a32f52729b72bb4b2d23172d0d5d1602a225d88
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-06 21:18:13 +0100

    fix meme

--- lolisafe.js
+++ lolisafe.js
@@ -62,7 +62,7 @@ if (config.serveFilesWithNode) {
 	safe.get('/:id', (req, res, next) => {
 		const id = req.params.id;
 		const ex = fs.existsSync('./' + config.uploads.folder + '/' + id);
-		if(!ex) return res.status(404).sendFile('404.html', { root: './pages/error/' }));
+		if(!ex) return res.status(404).sendFile('404.html', { root: './pages/error/' });
 		return res.sendFile('./' + config.uploads.folder + '/' + id);
 	});
 }
