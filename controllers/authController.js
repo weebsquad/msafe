9ae62da45533183a34a9396f19921ffa695b476b
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-07 20:30:24 +0100

    linted and bugfix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,219 +1,218 @@
-const config = require('../config.js');
-const db = require('knex')(config.database);
-const bcrypt = require('bcrypt');
-const randomstring = require('randomstring');
-const utils = require('./utilsController.js');
-const uploadController = require('./uploadController.js');
+const config = require('../config.js')
+const db = require('knex')(config.database)
+const bcrypt = require('bcrypt')
+const randomstring = require('randomstring')
+const utils = require('./utilsController.js')
+const uploadController = require('./uploadController.js')
 
-let authController = {};
+let authController = {}
 
 authController.verify = async (req, res, next) => {
-	const username = req.body.username;
-	const password = req.body.password;
+  const username = req.body.username
+  const password = req.body.password
 
-	if (username === undefined) return res.json({ success: false, description: 'No username provided' });
-	if (password === undefined) return res.json({ success: false, description: 'No password provided' });
+  if (username === undefined) return res.json({ success: false, description: 'No username provided' })
+  if (password === undefined) return res.json({ success: false, description: 'No password provided' })
 
-	const user = await db.table('users').where('username', username).first();
-	if (!user) return res.json({ success: false, description: 'Username doesn\'t exist' });
-	if (user.enabled === false || user.enabled === 0) return res.json({
-		success: false,
-		description: 'This account has been disabled'
-	});
+  const user = await db.table('users').where('username', username).first()
+  if (!user) return res.json({ success: false, description: 'Username doesn\'t exist' })
+  if (user.enabled === false || user.enabled === 0) {
+    return res.json({
+      success: false,
+      description: 'This account has been disabled'
+    })
+  }
 
-	bcrypt.compare(password, user.password, (err, result) => {
-		if (err) {
-			console.log(err);
-			return res.json({ success: false, description: 'There was an error' });
-		}
-		if (result === false) return res.json({ success: false, description: 'Wrong password' });
-		return res.json({ success: true, token: user.token });
-	});
-};
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (err) {
+      console.log(err)
+      return res.json({ success: false, description: 'There was an error' })
+    }
+    if (result === false) return res.json({ success: false, description: 'Wrong password' })
+    return res.json({ success: true, token: user.token })
+  })
+}
 
 authController.listAccounts = async (req, res, next) => {
-	const user = await utils.authorize(req, res);
-	if(user.username !== 'root') return res.json({ success: false, description: 'No permission!' });
-	let users = await db.table('users').whereNot('username', 'root').select('id', 'username', 'enabled', 'timestamp');
-	
-	return res.json({ success: true, users });
-	
-};
+  const user = await utils.authorize(req, res)
+  if (user.username !== 'root') return res.json({ success: false, description: 'No permission!' })
+  let users = await db.table('users').whereNot('username', 'root').select('id', 'username', 'enabled', 'timestamp')
+
+  return res.json({ success: true, users })
+}
 
 authController.disableAccount = async (req, res, next) => {
-	let bypassEnable = false;
-	const user = await utils.authorize(req, res);
-	if(user && user.username === 'root') bypassEnable = true;
-	
-	const username = req.body.username;
-	const password = req.body.password;
-	let state = req.body.state;
-	if(typeof(state) !== 'boolean' && typeof(state) !== 'number') return res.json({ success: false, description: 'No disable state provided!' });
-	if(state === 0) state = false;
-	if(state === 1) state = true;
-	
-	if (username === undefined) return res.json({ success: false, description: 'No username provided' });
-	if (password === undefined) return res.json({ success: false, description: 'No password provided' });
-	
-	if(username === user.username && user.username === 'root') {
-		return res.json({ success: false, description: 'Cannot disable root account!' });
-	}
-	bcrypt.compare(password, user.password, async (err, result) => {
-		if (err) {
-			console.log(err);
-			return res.json({ success: false, description: 'There was an error' });
-		}
-		if(result === false) return res.json({ success: false, description: 'Wrong password' });
-		let targ = user;
-		if(bypassEnable) {
-			targ = await db.table('users').where('username', username).first()
-			if(!targ) return res.json({ success: false, description: 'Couldn\'t find the target user!' });
-		}
-		if(!bypassEnable && username !== user.username) return res.json({ success: false, description: 'No permission to disable this user' });
-		
-		await db.table('users').where('id', targ.id).update({ enabled: state });
-		if(!state) {
-			const newtoken = randomstring.generate(64);
-			await db.table('users').where('token', targ.token).update({
-				token: newtoken,
-				timestamp: Math.floor(Date.now() / 1000)
-			});
-		}
-		
-		return res.json({success: true });
-		
-	});
+  let bypassEnable = false
+  const user = await utils.authorize(req, res)
+  if (user && user.username === 'root') bypassEnable = true
 
-};
+  const username = req.body.username
+  const password = req.body.password
+  let state = req.body.state
+  if (typeof (state) !== 'boolean' && typeof (state) !== 'number') return res.json({ success: false, description: 'No disable state provided!' })
+  if (state === 0) state = false
+  if (state === 1) state = true
 
+  if (username === undefined) return res.json({ success: false, description: 'No username provided' })
+  if (password === undefined) return res.json({ success: false, description: 'No password provided' })
+
+  if (username === user.username && user.username === 'root') {
+    return res.json({ success: false, description: 'Cannot disable root account!' })
+  }
+  bcrypt.compare(password, user.password, async (err, result) => {
+    if (err) {
+      console.log(err)
+      return res.json({ success: false, description: 'There was an error' })
+    }
+    if (result === false) return res.json({ success: false, description: 'Wrong password' })
+    let targ = user
+    if (bypassEnable) {
+      targ = await db.table('users').where('username', username).first()
+      if (!targ) return res.json({ success: false, description: 'Couldn\'t find the target user!' })
+    }
+    if (!bypassEnable && username !== user.username) return res.json({ success: false, description: 'No permission to disable this user' })
+
+    await db.table('users').where('id', targ.id).update({ enabled: state })
+    if (!state) {
+      const newtoken = randomstring.generate(64)
+      await db.table('users').where('token', targ.token).update({
+        token: newtoken,
+        timestamp: Math.floor(Date.now() / 1000)
+      })
+    }
+
+    return res.json({success: true })
+  })
+}
 
 authController.deleteAccount = async (req, res, next) => {
-	let bypassEnable = false;
-	const user = await utils.authorize(req, res);
-	if(user && user.username === 'root') bypassEnable = true;
-	
-	const username = req.body.username;
-	const password = req.body.password;
-	const filesOnly = req.body.filesonly || false;
-	
-	if (username === undefined) return res.json({ success: false, description: 'No username provided' });
-	if (password === undefined) return res.json({ success: false, description: 'No password provided' });
-	
-	if(username === user.username && user.username === 'root') {
-		return res.json({ success: false, description: 'Cannot delete root account!' });
-	}
-	bcrypt.compare(password, user.password, async (err, result) => {
-		if (err) {
-			console.log(err);
-			return res.json({ success: false, description: 'There was an error' });
-		}
-		if(result === false) return res.json({ success: false, description: 'Wrong password' });
-		let targ = user;
-		if(bypassEnable) {
-			targ = await db.table('users').where('username', username).first()
-			if(!targ) return res.json({ success: false, description: 'Couldn\'t find the target user!' });
-		}
-		if(!bypassEnable && username !== user.username) return res.json({ success: false, description: 'No permission to delete this user' });
-		
-		const newtoken = randomstring.generate(64);
-		if(!filesOnly) {
-			await db.table('users').where('id', targ.id).update({ enabled: 0 });
-			await db.table('users').where('token', targ.token).update({
-				token: newtoken,
-				timestamp: Math.floor(Date.now() / 1000)
-			});
-		}
-		const userFiles = await db.table('files')
-			.where(function () {
-				this.where('userid', targ.id)
-			})
-		for(let key in userFiles) {
-			let obj = userFiles[key];
-			if(obj['userid'] === targ.id) { 
-				uploadController.deleteFile(obj['name']);
-			}
-		}
-		await db.table('files').where('userid', targ.id).del()
-		await db.table('albums').where('userid', targ.id).del()
-		await db.table('users').where('id', targ.id).del()
-		
-		return res.json({success: true });
-		
-	});
-};
+  let bypassEnable = false
+  const user = await utils.authorize(req, res)
+  if (user && user.username === 'root') bypassEnable = true
+
+  const username = req.body.username
+  const password = req.body.password
+  const filesOnly = req.body.filesonly || false
+
+  if (username === undefined) return res.json({ success: false, description: 'No username provided' })
+  if (password === undefined) return res.json({ success: false, description: 'No password provided' })
+
+  if (username === user.username && user.username === 'root') {
+    return res.json({ success: false, description: 'Cannot delete root account!' })
+  }
+  bcrypt.compare(password, user.password, async (err, result) => {
+    if (err) {
+      console.log(err)
+      return res.json({ success: false, description: 'There was an error' })
+    }
+    if (result === false) return res.json({ success: false, description: 'Wrong password' })
+    let targ = user
+    if (bypassEnable) {
+      targ = await db.table('users').where('username', username).first()
+      if (!targ) return res.json({ success: false, description: 'Couldn\'t find the target user!' })
+    }
+    if (!bypassEnable && username !== user.username) return res.json({ success: false, description: 'No permission to delete this user' })
+
+    const newtoken = randomstring.generate(64)
+    if (!filesOnly) {
+      await db.table('users').where('id', targ.id).update({ enabled: 0 })
+      await db.table('users').where('token', targ.token).update({
+        token: newtoken,
+        timestamp: Math.floor(Date.now() / 1000)
+      })
+    }
+    const userFiles = await db.table('files')
+      .where(function () {
+        this.where('userid', targ.id)
+      })
+    for (let key in userFiles) {
+      let obj = userFiles[key]
+      if (obj['userid'] === targ.id) {
+        uploadController.deleteFile(obj['name'])
+      }
+    }
+    await db.table('files').where('userid', targ.id).del()
+    await db.table('albums').where('userid', targ.id).del()
+    await db.table('users').where('id', targ.id).del()
+
+    return res.json({success: true })
+  })
+}
 
 authController.register = async (req, res, next) => {
-	let bypassEnable = false;
-	const token = req.headers.token || '';
-	const _user = await db.table('users').where('token', token).first();
-	if(_user && _user.username === 'root') bypassEnable = true;
-	if (config.enableUserAccounts === false && !bypassEnable) {
-		return res.json({ success: false, description: 'Register is disabled at the moment' });
-	}
+  let bypassEnable = false
+  const token = req.headers.token || ''
+  const _user = await db.table('users').where('token', token).first()
+  if (_user && _user.username === 'root') bypassEnable = true
+  if (config.enableUserAccounts === false && !bypassEnable) {
+    return res.json({ success: false, description: 'Register is disabled at the moment' })
+  }
 
-	const username = req.body.username;
-	const password = req.body.password;
+  const username = req.body.username
+  const password = req.body.password
 
-	if (username === undefined) return res.json({ success: false, description: 'No username provided' });
-	if (password === undefined) return res.json({ success: false, description: 'No password provided' });
+  if (username === undefined) return res.json({ success: false, description: 'No username provided' })
+  if (password === undefined) return res.json({ success: false, description: 'No password provided' })
 
-	if (username.length < 4 || username.length > 32) {
-		return res.json({ success: false, description: 'Username must have 4-32 characters' });
-	}
-	if (password.length < 6 || password.length > 64) {
-		return res.json({ success: false, description: 'Password must have 6-64 characters' });
-	}
+  if (username.length < 4 || username.length > 32) {
+    return res.json({ success: false, description: 'Username must have 4-32 characters' })
+  }
+  if (password.length < 6 || password.length > 64) {
+    return res.json({ success: false, description: 'Password must have 6-64 characters' })
+  }
 
-	const user = await db.table('users').where('username', username).first();
-	if (user) return res.json({ success: false, description: 'Username already exists' });
+  const user = await db.table('users').where('username', username).first()
+  if (user) return res.json({ success: false, description: 'Username already exists' })
 
-	bcrypt.hash(password, 10, async (err, hash) => {
-		if (err) {
-			console.log(err);
-			return res.json({ success: false, description: 'Error generating password hash (╯°□°）╯︵ ┻━┻' });
-		}
-		const token = randomstring.generate(64);
-		await db.table('users').insert({
-			username: username,
-			password: hash,
-			token: token,
-			enabled: 1
-		});
-		return res.json({ success: true, token: token });
-	});
-};
+  bcrypt.hash(password, 10, async (err, hash) => {
+    if (err) {
+      console.log(err)
+      return res.json({ success: false, description: 'Error generating password hash (╯°□°）╯︵ ┻━┻' })
+    }
+    const token = randomstring.generate(64)
+    await db.table('users').insert({
+      username: username,
+      password: hash,
+      token: token,
+      enabled: 1
+    })
+    return res.json({ success: true, token: token })
+  })
+}
 
 authController.changePassword = async (req, res, next) => {
-	const user = await utils.authorize(req, res);
+  const user = await utils.authorize(req, res)
 
-	let password = req.body.password;
-	let random = req.body.random || false;
-	if(random === 0) random = false;
-	if(random === 1) random = true;
-	if (password === undefined && random === false) return res.json({ success: false, description: 'No password provided' });
-	if(random) password = randomstring.generate(8);
-	if (password.length < 6 || password.length > 64) {
-		return res.json({ success: false, description: 'Password must have 6-64 characters' });
-	}
+  let username = req.body.username
+  if (username === undefined) username = ''
+  let password = req.body.password
+  let random = req.body.random || false
+  if (random === 0) random = false
+  if (random === 1) random = true
+  if (password === undefined && random === false) return res.json({ success: false, description: 'No password provided' })
+  if (random) password = randomstring.generate(8)
+  if (password.length < 6 || password.length > 64) {
+    return res.json({ success: false, description: 'Password must have 6-64 characters' })
+  }
 
-	let bypassEnable = false;
-	if(user && user.username === 'root') bypassEnable = true;
-	bcrypt.hash(password, 10, async (err, hash) => {
-		if (err) {
-			console.log(err);
-			return res.json({ success: false, description: 'Error generating password hash (╯°□°）╯︵ ┻━┻' });
-		}
-		let targ = user;
-		if(bypassEnable) {
-			targ = await db.table('users').where('username', username).first()
-			if(!targ) return res.json({ success: false, description: 'Couldn\'t find the target user!' });
-		}
+  let bypassEnable = false
+  if (user && user.username === 'root') bypassEnable = true
+  bcrypt.hash(password, 10, async (err, hash) => {
+    if (err) {
+      console.log(err)
+      return res.json({ success: false, description: 'Error generating password hash (╯°□°）╯︵ ┻━┻' })
+    }
+    let targ = user
+    if (bypassEnable) {
+      targ = await db.table('users').where('username', username).first()
+      if (!targ) return res.json({ success: false, description: 'Couldn\'t find the target user!' })
+    }
 
-		await db.table('users').where('id', targ.id).update({ password: hash });
-		let ret = { success: true };
-		if(random) ret['newpw'] = password;
-		return res.json(ret);
-	});
-};
+    await db.table('users').where('id', targ.id).update({ password: hash })
+    let ret = { success: true }
+    if (random) ret['newpw'] = password
+    return res.json(ret)
+  })
+}
 
-module.exports = authController;
+module.exports = authController
