6bb516fe7324ac3b99f1f3b4575edfe1151b60be
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-11 19:45:32 +0100

    asd

--- routes/s3.js
+++ routes/s3.js
@@ -55,7 +55,7 @@ s3.initialize = function() {
 	s3['client'] = libs3.createClient(clientOpts);
 	s3['url'] = libs3.getPublicUrl(optionsS3.bucket, optionsS3.secretAccessKey);
 	console.log(s3);
-	s3.test(optionsS3.bucket);
+	//s3.test(optionsS3.bucket);
 };
 
 module.exports = s3;
