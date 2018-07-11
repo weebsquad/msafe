b48368e89981c336b8ce812677ea451e08a3c3ad
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-11 19:57:23 +0100

    idk

--- routes/s3.js
+++ routes/s3.js
@@ -41,10 +41,7 @@ s3.test = function(bucket) {
 		maxkeys: 0,
 		MaxKeys: 0,
 	};
-	s3.client.listObjects(params, function(err, data) {
-		if (err) console.log(err, err.stack); // an error occurred
-		else console.log(data);
-	});
+	let objects = s3.client.listObjects(params);
 };
 
 
