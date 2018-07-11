920f8d7d2bccb214cd24376e6cc02caf982a7b61
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-11 20:49:28 +0100

    tests

--- routes/s3.js
+++ routes/s3.js
@@ -15,6 +15,7 @@ const clientOpts = {
     accessKeyId: optionsS3.accessKey,
     secretAccessKey: optionsS3.secretAccessKey,
     region: optionsS3.region,
+	signatureVersion: 'v2',
     // endpoint: 's3.yourdomain.com',
     // sslEnabled: false
     // any other options are passed to new AWS.S3()
@@ -100,7 +101,7 @@ s3.initialize = async function() {
 	s3['client'] = libs3.createClient(clientOpts);
 	s3['url'] = libs3.getPublicUrl(optionsS3.bucket, optionsS3.secretAccessKey);
 	await s3.getFiles(optionsS3.bucket);
-	s3.uploadFile(optionsS3.bucket, 'ZyROE.png', '/opt/lolisafe/uploads/ZyROE.png');
+	s3.uploadFile(optionsS3.bucket, 'ZyROE.png', 'uploads/ZyROE.png');
 };
 
 module.exports = s3;
