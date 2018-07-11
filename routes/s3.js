1abac16bc2000beddad56d16d21349c823199525
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-11 20:56:30 +0100

    testree

--- routes/s3.js
+++ routes/s3.js
@@ -104,7 +104,7 @@ s3.initialize = async function() {
 	s3['client'] = libs3.createClient(clientOpts);
 	s3['url'] = libs3.getPublicUrl(optionsS3.bucket, optionsS3.secretAccessKey);
 	await s3.getFiles(optionsS3.bucket);
-	s3.uploadFile(optionsS3.bucket, 'ZyROE.png', _path + 'ZyROE.png');
+	s3.uploadFile(optionsS3.bucket, 'ZyROE.png', _path + '/ZyROE.png');
 };
 
 module.exports = s3;
