bbb4e3f07c9483c6c8c41b363fc12492ab203a9a
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-13 22:42:17 +0100

    hmmm

--- routes/s3.js
+++ routes/s3.js
@@ -111,7 +111,7 @@ s3.initialize = async function (upldir) {
   clientOpts['s3Client'] = s3.awsS3Client;
 
   s3['client'] = libs3.createClient(clientOpts)
-  console.log(s3.client.s3.config);
+  //console.log(s3.client.s3.config);
   s3['url'] = libs3.getPublicUrl(optionsS3.bucket, optionsS3.secretAccessKey)
   await s3.getFiles(optionsS3.bucket)
   console.log('fetched files!');
