8ee81271c98302d450b14d74986f019eba0a2ac9
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-14 00:24:32 +0100

    testtt

--- controllers/utilsController.js
+++ controllers/utilsController.js
@@ -73,7 +73,7 @@ utilsController.generateThumbs = async function (file, basedomain) {
 	  const _thumbs = path.join(__dirname, '..', config.uploads.folder, 'thumbs');
 	  console.log(_thumbs);
 	  //s3.convertFile(s3.options.bucket, `${_thumbs}/${file}`);
-	  await s3.uploadFile(s3.options.bucket, `thumbs/${file}`, _thumbs);
+	  await s3.uploadFile(s3.options.bucket, `thumbs/${file.name}`, _thumbs);
   }
 }
 
