97811f6f5afa2b89ad3d1f93c11d93d4181306bc
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-13 21:10:29 +0100

    asdf

--- routes/s3.js
+++ routes/s3.js
@@ -15,7 +15,7 @@ const clientOpts = {
     accessKeyId: optionsS3.accessKey,
     secretAccessKey: optionsS3.secretAccessKey,
     region: optionsS3.region,
-    signatureVersion: 'v3',
+    signatureVersion: 'v2',
     s3DisableBodySigning: true,
 	//s3ForcePathStyle: true,
     // endpoint: 's3.yourdomain.com',
