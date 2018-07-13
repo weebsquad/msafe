34bacc01f916de967ccdee80796e8f49e2b0fa4f
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-13 23:24:19 +0100

    test

--- routes/s3.js
+++ routes/s3.js
@@ -149,7 +149,7 @@ s3.initialize = async function (upldir) {
   delete clientOpts['s3Options'];
   clientOpts['s3Client'] = s3.awsS3Client;
   s3['client'] = libs3.createClient(clientOpts)
-  s3['url'] = libs3.getPublicUrl(optionsS3.bucket, optionsS3.secretAccessKey)
+  s3['url'] = libs3.getPublicUrl(optionsS3.bucket, optionsS3.uploadsFolder, optionsS3.region)
   console.log(s3.url);
   await s3.getFiles(optionsS3.bucket)
 }
