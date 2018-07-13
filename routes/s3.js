63d7cb69d5f197ee21000fa762a07f422f0df9a7
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-13 22:10:44 +0100

    test

--- routes/s3.js
+++ routes/s3.js
@@ -3,7 +3,6 @@ const fs = require('fs')
 const libs3 = require('s3')
 const db = require('knex')(config.database)
 const path = require('path')
-const AWS = require('aws-sdk');
 
 
 const optionsS3 = config.s3
@@ -17,12 +16,18 @@ const clientOpts = {
   s3Options: {
     accessKeyId: optionsS3.accessKey,
     secretAccessKey: optionsS3.secretAccessKey,
+	credentials: { 
+		accessKeyId: optionsS3.accessKey,
+		secretAccessKey: optionsS3.secretAccessKey,
+	},
     region: optionsS3.region,
     signatureVersion: 'v3',
     s3DisableBodySigning: true,
 	//s3ForcePathStyle: true,
     // endpoint: 's3.yourdomain.com',
     sslEnabled: false,
+	paramValidation: false,
+	
     // any other options are passed to new AWS.S3()
     // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
   }
@@ -31,7 +36,6 @@ const clientOpts = {
 
 
 let s3 = {}
-s3.awsS3Client = new AWS.S3(clientOpts['s3Options']);
 
 s3.enabledCheck = function () {
   if (optionsS3.use !== true || optionsS3.accessKey === '' || optionsS3.secretAccessKey === '') return false
@@ -106,9 +110,6 @@ s3.convertFile = async function (bucket, localPath) {
 
 s3.initialize = async function (upldir) {
   if (!s3.enabledCheck()) return
-  delete clientOpts['s3Options'];
-  clientOpts['s3Client'] = s3.awsS3Client;
-  console.log(clientOpts);
   s3['client'] = libs3.createClient(clientOpts)
   //console.log(s3.client.s3.config);
   s3['url'] = libs3.getPublicUrl(optionsS3.bucket, optionsS3.secretAccessKey)
