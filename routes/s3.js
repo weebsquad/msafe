60f92640ee9a418f0ff24db3da474272bf162805
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-14 12:12:48 +0100

    remove debugging

--- routes/s3.js
+++ routes/s3.js
@@ -92,7 +92,7 @@ s3.uploadFile = async function (bucket, fileName, localPath) {
 		  reject(err)
     })
     uploader.on('end', async function () {
-		  console.log('done uploading')
+		  //console.log('done uploading')
 		  await s3.getFiles(s3.options.bucket)
 		  resolve(true)
     })
@@ -153,7 +153,7 @@ s3.deleteFiles = async function (bucket, files) {
 		  reject(err)
     })
     deleter.on('end', function () {
-		  console.log('done deleting')
+		  //console.log('done deleting')
 		  // await s3.getFiles()
 		  for (var i = 0; i < s3.files.length; i++) {
 			  let vl = s3.files[i]
