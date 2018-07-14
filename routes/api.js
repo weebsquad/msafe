22afa09f0ed86e22220f62aeec7903f640c5adeb
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-14 18:06:57 +0100

    test

--- routes/api.js
+++ routes/api.js
@@ -5,15 +5,56 @@ const albumsController = require('../controllers/albumsController')
 const tokenController = require('../controllers/tokenController')
 const authController = require('../controllers/authController')
 
-routes.get('/check', (req, res, next) => {
-  return res.json({
-    private: config.private,
-    maxFileSize: config.uploads.maxSize,
-    register: config.enableUserAccounts
+function check(req, res, next) {
+	return res.json({
+		private: config.private,
+		maxFileSize: config.uploads.maxSize,
+		register: config.enableUserAccounts
   })
-})
+}
 
-routes.get('/admins', (req, res, next) => authController.listAdmins(req, res, next))
+
+const map = {
+	'get': {
+		'check': check,
+		'admins': authController.listAdmins,
+		'account/list': authController.listAccounts,
+		'uploads': uploadController.list,
+		'uploads/:page': uploadController.list,
+		'album/get/:identifier': albumsController.get,
+		'album/zip/:identifier': albumsController.generateZip,
+		'album/:id': uploadController.list,
+		'album/:id/:page': uploadController.list,
+		'albums': albumsController.list,
+		'albums/:sidebar': albumsController.list,
+//		'albums/test': albumsController.test,
+		'tokens': tokenController.list,
+	},
+	'post': {
+		'login': authController.verify,
+		'register': authController.register,
+		'account/delete': authController.deleteAccount,
+		'account/disable': authController.disableAccount,
+		'password/change': authController.changePassword,
+		'upload': uploadController.upload,
+		'upload/delete': uploadController.delete,
+		'upload/:albumid': uploadController.upload,
+		'albums': albumsController.create,
+		'albums/delete': albumsController.delete,
+		'albums/rename': albumsController.rename,
+		'tokens/verify': tokenController.verify,
+		'tokens/change': tokenController.change,
+	}
+};
+
+for(let type in map) {
+	for(let key in map[type]) {
+		let obj = map[type][key];
+		routes[type](`/${key}`, (req, res, next) => obj(req, res, next))
+	}
+}
+
+/*routes.get('/admins', (req, res, next) => authController.listAdmins(req, res, next))
 routes.post('/login', (req, res, next) => authController.verify(req, res, next))
 routes.post('/register', (req, res, next) => authController.register(req, res, next))
 routes.post('/account/delete', (req, res, next) => authController.deleteAccount(req, res, next))
@@ -37,6 +78,6 @@ routes.post('/albums/rename', (req, res, next) => albumsController.rename(req, r
 routes.get('/albums/test', (req, res, next) => albumsController.test(req, res, next))
 routes.get('/tokens', (req, res, next) => tokenController.list(req, res, next))
 routes.post('/tokens/verify', (req, res, next) => tokenController.verify(req, res, next))
-routes.post('/tokens/change', (req, res, next) => tokenController.change(req, res, next))
+routes.post('/tokens/change', (req, res, next) => tokenController.change(req, res, next))*/
 
 module.exports = routes
