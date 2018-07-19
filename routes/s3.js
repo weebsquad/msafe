f51eaf165fc922d55aad8e2167bad5e6e3c1b6cb
MetalRuller <MetalRuller@users.noreply.github.com>
2018-07-19 19:22:59 +0100

    okk

--- routes/s3.js
+++ routes/s3.js
@@ -105,8 +105,9 @@ s3.uploadFile = async function (bucket, fileName, localPath, dbId = '') {
 		  await s3.getFiles(s3.options.bucket)
 		  console.log(dbId)
 		  console.log(expdate);
-		  let fl = db.table('files').where('name', dbId);
-		  fl.update({ timestampExpire: expdate });
+		  let fl = await db.table('files').where('name', dbId);
+		  await fl.update({ timestampExpire: expdate });
+		  fl = await db.table('files').where('name', dbId).first();
 		  console.log(fl);
 		  resolve(true)
     })
