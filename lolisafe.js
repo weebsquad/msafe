95284d73cb70fc96c85c9ce5caf988fe1fa379b5
MetalRuller <MetalRuller@users.noreply.github.com>
2019-06-15 17:46:05 +0100

    debug

--- lolisafe.js
+++ lolisafe.js
@@ -190,9 +190,11 @@ let init = async function (reload = false) {
 	  doCrons();
   }
   await require('./database/db.js')(db)
+  console.log('Loaded DB');
   const _path = path.join(__dirname, config.uploads.folder)
   let fl = await db.table('files').select('name')
   await s3.initialize(_path, fl)
+  console.log('Loaded S3');
   setupExpress(_safenew, reload);
   if(reload && serv) { serv.close(); delete serv; delete safeog; }
   safeog = _safenew
