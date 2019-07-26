c2a69356c3b3d266708af7108cb78a84f8fce18d
MetalRuller <admin@metalruller.com>
2019-07-26 15:04:38 +0100

    Update db.js

--- database/db.js
+++ database/db.js
@@ -120,74 +120,20 @@ let init = async function(db){
 			}
 		}
 		
+
+		
+		handleActions(tableName);
+		
 		// Do indexes
 		if(typeof(tableDef['indexes']) === 'object' && tableDef['indexes'].length > 0) {
 			await db.schema.table(tableName, function(tableObject) {
+				console.log(tableObject.columns);
 				//tableObject.index(tableDef['indexes']);
-				tableObject.dropIndex(tableDef['indexes']);
+				//tableObject.dropIndex(tableDef['indexes']);
 				//console.log(`[DB] ${tableName} - Adding indexes`);
 			});
 		}
-		
-		handleActions(tableName);
 	}
-	
-	/*
-	// Create the tables we need to store galleries and files
-	let _ex = await db.schema.hasTable('albums')
-	if(!_ex) db.schema.createTable('albums', function (table) {
-		table.increments();
-		table.integer('userid');
-		table.string('name');
-		table.string('identifier');
-		table.integer('enabled');
-		table.integer('timestamp');
-		table.integer('editedAt');
-		table.integer('zipGeneratedAt');
-	}).then(() => {});
-
-	_ex = await db.schema.hasTable('files')
-	if(!_ex) db.schema.createTableIfNotExists('files', function (table) {
-		table.increments();
-		table.integer('userid');
-		table.string('name');
-		table.string('original');
-		table.string('type');
-		table.string('size');
-		table.string('hash');
-		table.string('ip');
-		table.integer('albumid');
-		table.integer('timestamp');
-		table.integer('timestampExpire');
-		table.integer('encodeVersion');
-		table.string('encodedString');
-	}).then(() => {});
-	
-	_ex = await db.schema.hasTable('users')
-	if(!_ex) db.schema.createTableIfNotExists('users', function (table) {
-		table.increments();
-		table.string('username');
-		table.string('password');
-		table.string('token');
-		table.integer('enabled');
-		table.integer('timestamp');
-	}).then(() => {
-		db.table('users').where({username: 'root'}).then((user) => {
-			if(user.length > 0) return;
-
-			require('bcrypt').hash('root', 10, function(err, hash) {
-				if(err) console.error('Error generating password hash for root');
-
-				db.table('users').insert({
-					username: 'root',
-					password: hash,
-					token: require('randomstring').generate(64),
-					timestamp: Math.floor(Date.now() / 1000)
-				}).then(() => {});
-			});
-		});
-	});
-	*/
 };
 
 module.exports = init;
